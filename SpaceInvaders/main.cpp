f44e0859367e24d2d868a771a1bdffe445b456ff
NanocomStudios <dulararupasinghe@gmail.com>
2024-11-26 14:47:37 +0530

    Objects

--- SpaceInvaders/main.cpp
+++ SpaceInvaders/main.cpp
@@ -1,6 +1,8 @@
 ﻿#include <iostream>
 #include <conio.h>
 #include <time.h>
+#include <stdlib.h>
+
 using namespace std;
 
 short bottom = 24;
@@ -81,29 +83,137 @@ public:
 
 };
 
-int inp;
+class eShip {
+public:
+    short x = 10;
+    short y = 90;
 
+    short state = 0;
+
+    void draw(short newX, short newY) {
+        moveCsr(x - 1, y - 3);
+        cout << "       ";
+        moveCsr(newX - 1 , newY - 3);
+        cout << "\\=====/";
+
+        moveCsr(x , y - 3);
+        cout << "       ";
+        moveCsr(newX , newY - 2);
+        cout << "\\ X /";
+
+        moveCsr(x +1, y - 3);
+        cout << "       ";
+        moveCsr(newX +1, newY - 1);
+        cout << "\\ /";
+
+        moveCsr(x +2, y - 3);
+        cout << "       ";
+        moveCsr(newX +2, newY);
+        cout << ".";
+
+        x = newX;
+        y = newY;
+    }
+
+    void destroy() {
+        moveCsr(x - 1, y - 3);
+        cout << "\\88888/";
+
+        moveCsr(x, y - 3);
+        cout << "8\\888/8";
+
+        moveCsr(x + 1, y - 3);
+        cout << "88\\X/88";
+
+        moveCsr(x + 2, y - 3);
+        cout << " 8 8 8 ";
+        state = 1;
+    }
+
+    void remove() {
+        moveCsr(x - 1, y - 3);
+        cout << "       ";
+
+        moveCsr(x, y - 3);
+        cout << "       ";
+
+        moveCsr(x + 1, y - 3);
+        cout << "       ";
+
+        moveCsr(x + 2, y - 3);
+        cout << "       ";
+        state = 2;
+    }
+
+};
+
+int inp;
+clock_t previousClock;
+clock_t currentClock;
 int main()
 {
     Player player;
+    eShip eShips[7];
 
     drawPlayField();
     player.draw(40);
-    
+
+    for (int i = 0; i < 7; i++) {
+        eShips[i].draw(10, 10 + (i * 10));
+    }
+
+    int destroyShip = 0;
+    int selectedShip = 0;
+
+    previousClock = clock();
+
     while (1) {
-        inp = _getch();
-        if (inp == 77) {
+        if (_kbhit()) {
+            inp = _getch();
 
-            if (player.position < 77) {
-                player.draw(player.position + 1);
+            if (inp == 77) {
+
+                if (player.position < 77) {
+                    player.draw(player.position + 1);
+                }
+            }
+            else if (inp == 75) {
+
+                if (player.position > 6) {
+                    player.draw(player.position - 1);
+                }
+            }
+            else if (inp == 32) {
+                short count = 0;
+                while (1) {
+                    selectedShip = rand() % 7;
+                    if (eShips[selectedShip].state == 0) {
+                        break;
+                    }
+                }
+                destroyShip = 1;
+
             }
         }
-        else if (inp == 75) {
 
-            if (player.position > 6) {
-                player.draw(player.position - 1);
+        currentClock = clock();
+
+        if ((currentClock - previousClock) > 300) {
+            if (destroyShip == 1) {
+                
+                if (eShips[selectedShip].state == 0) {
+                    eShips[selectedShip].destroy();
+                }
+                else {
+                    for (int i = 0; i < 7; i++) {
+                        if (eShips[i].state == 1) {
+                            eShips[i].remove();
+                        }
+                    }
+                }
             }
+            previousClock = currentClock;
         }
-        
+
     }
 }
