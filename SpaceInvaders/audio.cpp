ac5bde640c2fb423c97d6061c59d3aaed5307697
dulararupasinghe@gmail.com <dulararupasinghe@gmail.com>
2024-12-02 22:10:21 +0530

    Audio Update

--- SpaceInvaders/audio.cpp
+++ SpaceInvaders/audio.cpp
@@ -6,7 +6,7 @@
 #pragma comment(lib, "Winmm.lib")
 
 using namespace std;
-
+//https://stackoverflow.com/questions/22253074/how-to-play-or-open-mp3-or-wav-sound-file-in-c-program
 void playGameAudio() {
 	mciSendString("open \"song.mp3\" type mpegvideo alias mp3", NULL, 0, NULL);
 	mciSendString("play mp3", NULL, 0, NULL);
